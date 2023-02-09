43cac28d000aab9468f7cb3081d11db180369edd
celldns <90262514+celldns@users.noreply.github.com>
2023-02-09 20:43:53 +0800

    Update BlogPostCard.js

--- themes/matery/components/BlogPostCard.js
+++ themes/matery/components/BlogPostCard.js
@@ -38,53 +38,7 @@ const BlogPostCard = ({ post, showSummary, siteInfo }) => {
                   </Link>
               )}
 
-              <div >
-                  {/* 描述 */}
-                  <div className="px-4 flex flex-col w-full  text-gray-700  dark:text-gray-300">
 
-                      {(!showPreview || showSummary) && post.summary && (
-                          <p style={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: '4', WebkitBoxOrient: 'vertical' }}
-                              className="replace my-2 text-sm font-light leading-7">
-                              {post.summary}
-                          </p>
-                      )}
-
-                      <div className='text-gray-800 justify-between flex my-2  dark:text-gray-300'>
-                          <Link
-                              href={`/archive#${post?.date?.start_date?.substr(0, 7)}`}
-                              passHref
-                              className="font-light hover:underline cursor-pointer text-sm leading-4 mr-3">
-
-                              <i className="far fa-clock mr-1" />
-                              {post.date?.start_date || post.lastEditedTime}
-
-                          </Link>
-                          <Link
-                              href={`/category/${post.category}`}
-                              passHref
-                              className="cursor-pointer font-light text-sm hover:underline hover:text-indigo-700 dark:hover:text-indigo-400 transform">
-
-                              <i className="mr-1 far fa-folder" />
-                              {post.category}
-
-                          </Link>
-                      </div>
-                  </div>
-
-                  {post?.tagItems && post?.tagItems.length > 0 && (<>
-                      <hr />
-                      <div className="text-gray-400 justify-between flex px-5 py-3">
-                          <div className="md:flex-nowrap flex-wrap md:justify-start inline-block">
-                              <div>
-                                  {' '}
-                                  {post.tagItems.map(tag => (
-                                      <TagItemMini key={tag.name} tag={tag} />
-                                  ))}
-                              </div>
-                          </div>
-                      </div>
-                  </>)}
-              </div>
           </div>
 
       </div>
